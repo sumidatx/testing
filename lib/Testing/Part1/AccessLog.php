edc39d0e786e37ef5956ee1f73c723da61230a84
usoda.dondokodoon <usoda.dondokodoon>
2013-08-28 16:24:22 +0900

    initialize: part1

---
+++ lib/Testing/Part1/AccessLog.php
@@ -0,0 +1,25 @@
+<? namespace Testing\Part1;
+/**
+ * AccessLog
+ *
+ * @package
+ * @version
+ * @copyright
+ * @author
+ * @licence
+ */
+class AccessLog {
+  /**
+   * __construct__
+   *
+   */
+  public function __construct($path) {}
+  /**
+   * lineCountByStatusCode
+   *
+   * @param mixed $status_code
+   * @access public
+   * @return integer
+   */
+  public function getLineCountByStatusCode($status_code) {}
+}
