48a43d684fb8310d2a389d76a82c08ba6bd875cd
Hiroshi Sumida <sumida.hiroshi@techcross.co.jp>
2013-09-02 18:58:11 +0900

    テストコード追加

--- part1/test/sample.php
+++ part1/test/sample.php
@@ -3,17 +3,128 @@ require_once __DIR__."/../../vendor/autoload.php";
 
 class SampleTest extends PHPUnit_Framework_TestCase
 {
-  public function testSample()
-  {
-    $got = "hoge";
-    $expected = "hoge";
-    $this->assertEquals($got, $expected);
-  }
+	const FILEPATH = '../data/access_log';
+	private $status_codes = array(100 => 100, 101 => 101,
+			200 => 200, 201 => 201, 202 => 202, 203 => 203, 204 => 204, 205 => 205, 206 =>206,
+			300 => 300, 301 => 301, 302 => 302, 303 => 303, 304 => 304, 305 => 305,
+			400 => 400, 401 => 401, 402 => 402, 403 => 403, 404 => 404, 405 => 405, 406 => 406, 407 => 407, 408 => 408, 409 => 409, 410 => 410, 411 => 411, 412 => 412, 413 => 413, 414 => 414, 415 =>415,
+			500 => 500, 501 => 501, 502 => 502, 503 => 503, 504 => 504, 505 => 505,);
 
-  public function testInvalidSample()
-  {
-    $got = "ihoge";
-    $expected = "hoge";
-    $this->assertEquals($got, $expected, "同じ値でないといけない");
-  }
+	public function setUp() {
+		$this->access_log =  new \Testing\Part1\AccessLog(self::FILEPATH);
+		$this->file_pointer = fopen(self::FILEPATH);
+	}
+
+	public function test有効なパスの初期化テスト()
+	{
+		try {
+			$access_log = new \Testing\Part1\AccessLog(self::FILEPATH);
+		}
+		catch(Exception $e)
+		{
+			$this->fail($message = '有効なパスで初期化失敗' . $e->getMessage());
+			return;
+		}
+
+		$this->assertTrue(true, $message = '有効なパスで初期化成功');
+	}
+
+	public function test無効なパスの初期化テスト()
+	{
+		try {
+			$access_log = new \Testing\Part1\AccessLog($path = '../data/hogehoge_log');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '無効なパスで初期化失敗' . $e->getMessage());
+			return;
+		}
+
+		$this->fail($message = '無効なパスで初期化成功');
+	}
+	
+	public function test不正なパスの初期化テスト()
+	{
+		try {
+			$access_log = new \Testing\Part1\AccessLog($path = '@@93843=^^^\003hogehoge');
+			$this->fail($message = '不正なパス(文字列)で初期化失敗');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '不正なパス(文字列)で初期化成功', $e->getMessage());
+		}
+
+		try{
+			$access_log = new \Testing\Part1\AccessLog($path = '');
+			$this->fail($message = '不正なパスで初期化失敗');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '不正なパスで初期化成功', $e->getMessage());
+		}
+
+		try{
+			$access_log = new \Testing\Part1\AccessLog($this->file_pointer);
+			$this->fail($message = '不正なパスで初期化失敗');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '不正なパスで初期化成功', $e->getMessage());
+		}
+
+		try{
+			$access_log = new \Testing\Part1\AccessLog($path = 12345);
+			$this->fail($message = '不正なパスで初期化失敗');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '不正なパス(数値)で初期化成功', $e->getMessage());
+		}
+	}
+
+	public function test正常にステータスコードをカウントできたぜ()
+	{
+		foreach($this->status_codes as $status_code => $expected)
+		{
+			$acctual = $this->access_log->getLineCountByStatusCode($status_code);
+			$this->assertEquals($expected, $acctual, $message = '失敗');
+		}
+	}
+
+	public function test無効なステータスコードが入力されましてよ()
+	{
+		try
+		{
+			$count = $this->access_log->getLineCountByStatusCode(3333);
+			$this->fail($message = '無効なステータスコードが入力されたのに例外が発生しなかった');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '無効なステータスコードが入力されたので例外発生成功');
+		}
+
+		try
+		{
+			$count = $this->access_log->getLineCountByStatusCode('hogehoge');	
+			$this->fail($message = '無効なステータスコードが入力されたのに例外が発生しなかった');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '無効なステータスコードが入力されたので例外発生成功');
+		}
+
+		try
+		{
+			$count = $this->access_log->getLineCountByStatusCode($this->file_pointer);
+			$this->fail($message = '無効なステータスコードが入力されたのに例外が発生しなかった');
+		}
+		catch(Exception $e)
+		{
+			$this->assertTrue(true, $message = '無効なステータスコードが入力されたので例外発生成功');
+		}
+	}
+
+	public function tearDown() {
+		fclose($this->file_pointer);
+	}
 }
