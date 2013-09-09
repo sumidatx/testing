5c1ca02df3c6877913e0767942459df7002c4c14
tatemi <tatemi.hiroki@techcross.co.jp>
2013-09-09 17:25:56 +0900

    add unit test part2

---
+++ lib/Testing/Part2/Sort.php
@@ -0,0 +1,33 @@
+<?php namespace Testing\Part2;
+
+/**
+ * ソートライブラリです。
+ * 配列を受け取ると、ソートして返します。
+ * 配列以外は受け取りません。
+ * 配列の中身は数値と文字のみ許可します。
+ * 数値と文字が混在している場合、昇順であれば数値の後に文字が、降順であれば文字の後に数値がきます。
+ * 
+ * @author tatemi
+ *
+ */
+class Sort
+{
+  /**
+   * 昇順にソートします。
+   * 
+   * @param array $array
+   */
+  public function sort(array $array)
+  {
+    
+  }
+  
+  /**
+   * 降順にソートします。
+   * @param array $array
+   */
+  public function rsort(array $array)
+  {
+    
+  }
+}
