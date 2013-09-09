0406a884270de9d4dc3e3036c48b3395ef64dd05
takanori.koyanagi <koyanagi.takanori@techcross.co.jp>
2013-09-09 16:48:14 +0900

    try catchをやめる

--- lib/Testing/Part1/AccessLog.php
+++ lib/Testing/Part1/AccessLog.php
@@ -31,33 +31,25 @@ class AccessLog {
    */
   public function getLineCountByStatusCode($status_code) {
 
-    try {
+    // 入力チェック
+    if (!is_numeric($status_code) || strlen($status_code) != 3) {
+      throw new \Exception('数値３桁で入力して下さい');
+    }
 
-      // 入力チェック
-      if (!is_numeric($status_code) || strlen($status_code) != 3) {
-        throw new \Exception('数値３桁で入力して下さい');
-      }
-
-      $count = 0;
-      $serch_str = 'HTTP/1.1" '.$status_code;
-      if ($fp = fopen($this->path, 'r')) {
-        if (flock($fp, LOCK_SH)) {
-          while (!feof($fp)) {
-            $buffer = fgets($fp);
-            if (strpos($buffer, $serch_str)) {
-              $count++;
-            }
+    $count = 0;
+    $serch_str = 'HTTP/1.1" '.$status_code;
+    if ($fp = fopen($this->path, 'r')) {
+      if (flock($fp, LOCK_SH)) {
+        while (!feof($fp)) {
+          $buffer = fgets($fp);
+          if (strpos($buffer, $serch_str)) {
+            $count++;
           }
         }
-        fclose($fp);
-      } else {
-        throw new \Exception('ファイルが開けません');
       }
-
-    }
-    catch(Exception $e)
-    {
-      echo $e->getMessage();
+      fclose($fp);
+    } else {
+      throw new \Exception('ファイルが開けません');
     }
 
     return $count;
