84c7c84e1480a4d496391f1f2afa6aba4e6dc865
takanori.koyanagi <koyanagi.takanori@techcross.co.jp>
2013-10-03 12:10:33 +0900

    モックの修正完了

--- part3/test/sample.php
+++ part3/test/sample.php
@@ -18,14 +18,15 @@ class SampleTest extends PHPUnit_Framework_TestCase
   /*
    * @dataProvider provider_作成_OK
    */
-  public function test作成_OK($input, $msg, $expected)
+  public function test作成_OK($input, $mock_args, $msg, $expected)
   {
-    $stub = $this->getMock('\Testing\Part3\DB')
-                 ->expects($this->any())
-                 ->method('method_insert')
-                 ->will($this->returnValue($expected));
-    echo get_class($stub);
-    \Testing\Part3\DI::bind('DB', $stub);
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
+         ->method('method_insert')
+         ->with($this->equalTo($mock_args))
+         ->will($this->returnValue($expected));
+    \Testing\Part3\DI::bind('DB', $mock);
+
     $result = $this->card_manager->create($input);
     $actual = get_object_vars($result);
     $this->assertEquals($expected, $actual, $msg);
@@ -33,117 +34,158 @@ class SampleTest extends PHPUnit_Framework_TestCase
 
   public function provider_作成_OK()
   {
+    // 実際はinputによって変わる
+    $mock_args = '';
+
     return array(
       array(
         array('name' => 'カード1', 'description' => '説明1'),
+        $mock_args,
         '1件目のレコード',
         array('id' => 1,'name' => 'カード1', 'description' => '説明1')
       ),
       array(
         array('name' => 'カード2', 'description' => '説明2'),
+        $mock_args,
         '2件目のレコード(連番チェック)',
         array('id' => 2,'name' => 'カード2', 'description' => '説明2')
       ),
       array(
         array('name' => 'カード3'),
+        $mock_args,
         'descriptionが無い',
         array('id' => 3, 'name' => 'カード3'),
       ),
       array(
         array('name' => 'カード4', 'description' => '説明4', 'title' => 'タイトル'),
+        $mock_args,
         '存在しないプロパティ(title)を含む',
         array('id' => 4, 'name' => 'カード4', 'description' => '説明4', 'title' => 'タイトル'),
       )
     );
   }
 
-//  /*
-//   * @dataProvider provider_作成_NG
-//   */
-//  public function test作成_NG($input, $msg)
-//  {
-//    $stub = $this->getMock('\Testing\Part3\DB')
-//                 ->expects($this->never())
-//                 ->method('method_insert')
-//                 ->will($this->throwException(new \InvalidArgumentException()));
-//    \Testing\Part3\DI::bind('DB', $stub);
-//
-//    try {
-//      if (array_key_exists('input', $input)) {
-//        $this->card_manager->create($input['input']);
-//      } else {
-//        $this->card_manager->create();
-//      }
-//      $this->fail($msg);
-//    } catch (Exception $e) {
-//      $this->assertTrue(true, $msg);
-//      $this->assertEquals('受け取った配列に入力必須のプロパティが無いじゃん', $e->getMessage());
-//    }
-//  }
-//
-//  public function provider_作成_NG()
-//  {
-//    return array(
-//      array(array('dummy' => 'dummy'),                        '入力値が無い'),
-//      array(array('input' => null),                           '入力値がnull'),
-//      array(array('input' => array()),                        '入力値が空配列'),
-//      array(array('input' => array('description' => '説明')), '入力必須プロパティ(name)が無い'),
-//    );
-//  }
-//
-//  public function test読み取り_OK()
-//  {
-//    $okCaseArray = array(
-//      // id => array(input, msg)
-//      1 => array(
-//        array('name' => 'カード1', 'description' => '説明1'),
-//        '存在するidを指定',
-//      ),
-//    );
-//
-//    foreach ($okCaseArray as $id => $case) {
-//      list($input, $msg) = $case;
-//      $expected = $input + array('id' => $id);
-//      $stub = $this->getMock('\Testing\Part3\DB')
-//                   ->expects($this->any())
-//                   ->method('method_select')
-//                   ->will($this->returnValue($expected));
-//      \Testing\Part3\DI::bind('DB', $stub);
-//      $this->card_manager->create($input);
-//      $result = $this->card_manager->read($id);
-//      $actual = get_object_vars($result);
-//      $this->assertEquals($expected, $actual, $msg);
-//    }
-//  }
-//
-//  public function test読み取り_NG()
-//  {
-//    $ngCaseArray = array(
-//      // array(id, expects, msg)
-//      array(1,      '存在しないidを指定', 1, '$idが数値ではなかったよねー'),
-//      array('hoge', '数値以外を指定',     0, 'idが存在しないだろ、てやんでぃ'),
-//    );
-//
-//    foreach ($ngCaseArray as $case) {
-//      list($id, $msg, $expects, $error_msg) = $case;
-//      $stub = $this->getMock('\Testing\Part3\DB')
-//                   ->expects($this->at($expects))
-//                   ->method('method_select')
-//                   ->will($this->throwException(new \InvalidArgumentException()));
-//      \Testing\Part3\DI::bind('DB', $stub);
-//      try {
-//        $this->card_manager->read($id);
-//        $this->fail($msg);
-//      } catch (Exception $e) {
-//        $this->assertEquals($error_msg, $e->getMessage(), $msg);
-//      }
-//    }
-//  }
-//
+  public function test作成_NG()
+  {
+    $msg = '作成NG';
+    $input = array('description' => '説明');
+    $error_msg = '受け取った配列に入力必須のプロパティが無いじゃん';
+    try {
+      $this->card_manager->create($input);
+      $this->fail($msg);
+    } catch (Exception $e) {
+      $this->assertEquals($error_msg, $e->getMessage(), $msg);
+    }
+  }
+
+  // 引数チェック
+  /*
+   * @dataProvider provider_作成_NG
+   */
+  public function _test作成_NG($input, $msg)
+  {
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->never())
+         ->method('method_insert');
+    \Testing\Part3\DI::bind('DB', $mock);
+
+    try {
+      if (array_key_exists('input', $input)) {
+        $this->card_manager->create($input['input']);
+      } else {
+        $this->card_manager->create();
+      }
+      $this->fail($msg);
+    } catch (Exception $e) {
+      $this->assertTrue(true, $msg);
+      $this->assertEquals('受け取った配列に入力必須のプロパティが無いじゃん', $e->getMessage());
+    }
+  }
+
+  public function provider_作成_NG()
+  {
+    return array(
+      array(array('dummy' => 'dummy'),                        '入力値が無い'),
+      array(array('input' => null),                           '入力値がnull'),
+      array(array('input' => array()),                        '入力値が空配列'),
+      array(array('input' => array('description' => '説明')), '入力必須であるnameが無い'),
+    );
+  }
+
+  public function test読み取り_OK()
+  {
+    $msg = '読み取りOK';
+    $query = '';
+    $expected = array('id' => 1, 'name' => 'カード1', 'description' => '説明1');
+
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
+         ->method('method_select')
+         ->with($this->equalTo($query))
+         ->will($this->returnValue($expected));
+    \Testing\Part3\DI::bind('DB', $mock);
+    $result = $this->card_manager->read(1);
+    $actual = get_object_vars($result);
+    $this->assertEquals($expected, $actual, $msg);
+  }
+
+  public function test読み取り_NG()
+  {
+    $msg = '数値以外を指定';
+    $error_msg = '$idが数値ではなかったよねー';
+    try {
+      $this->card_manager->read('hoge');
+      $this->fail($msg);
+    } catch (Exception $e) {
+      $this->assertEquals($error_msg, $e->getMessage(), $msg);
+    }
+
+
+    $msg = '存在しないidを指定';
+    $error_msg = 'idが存在しないだろ、てやんでぃ';
+    $query = '';
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
+         ->method('method_select')
+         ->with($this->equalTo($query))
+         ->will($this->returnValue(null));
+    \Testing\Part3\DI::bind('DB', $mock);
+    try {
+      $this->card_manager->read(1);
+      $this->fail($msg);
+    } catch (Exception $e) {
+      $this->assertEquals($error_msg, $e->getMessage(), $msg);
+    }
+  }
+
+  // 実際には以下の観点でのテストも必要だが省略
+  // ・引数が両方無いとき(引数0)
+  // ・idが無いとき(引数1つ)
+  // ・paramsが無いとき(引数1つ)
+  // ・idにnullが指定されているとき
+  // ・paramsにnullが指定されているとき
   public function test更新_OK()
   {
-    // TODO: 引数のチェックをする
-    // TODO: モックの引数として指定される値もチェックする必要があるが現在は空文字を渡している
+    $msg = '更新OK';
+    $params = array();
+    $query = '';
+    $return_value = array();
+    $expected = array('id' => null, 'name' => null, 'description' => null);
+
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
+         ->method('method_update')
+         ->with($this->equalTo($query))
+         ->will($this->returnValue($return_value));
+    \Testing\Part3\DI::bind('DB', $mock);
+
+    $result = $this->card_manager->update(1, $params);
+    $actual = get_object_vars($result);
+    $this->assertEquals($expected, $actual, $msg);
+  }
+
+  public function test更新_NG()
+  {
     $msg = '更新NG(更新値にidが設定されている)';
     $params = array('id' => 1);
     try {
@@ -153,98 +195,52 @@ class SampleTest extends PHPUnit_Framework_TestCase
       $this->assertEquals('idは更新できません', $e->getMessage(), $msg);
     }
 
+
     $msg = '更新NG(該当データ無し)';
     $params = array();
-    $stub = $this->getMock('\Testing\Part3\DB');
-    $stub->expects($this->once())
+    $query = '';
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
          ->method('method_update')
+         ->with($this->equalTo($query))
          ->will($this->returnValue(null));
-    \Testing\Part3\DI::bind('DB', $stub);
+    \Testing\Part3\DI::bind('DB', $mock);
     try {
       $this->card_manager->update(1, $params);
       $this->fail($msg);
     } catch (\Exception $e) {
       $this->assertEquals('idが存在しない', $e->getMessage(), $msg);
     }
-
-    $msg = '更新値OK';
-    $params = array();
-    $return_value = array();
-    $expected = array('id' => null, 'name' => null, 'description' => null);
-    $stub = $this->getMock('\Testing\Part3\DB');
-    $stub->expects($this->once())
-         ->method('method_update')
-         ->will($this->returnValue($return_value));
-    \Testing\Part3\DI::bind('DB', $stub);
-    $result = $this->card_manager->update(1, $params);
-    $actual = get_object_vars($result);
-    $this->assertEquals($expected, $actual, $msg);
   }
-//
-//  public function test更新_NG()
-//  {
-//    $ngCaseArray = array(
-//      // id => array(input, update, msg, expects, error_msg)
-//      array(
-//        array('name' => 'カード1', 'description' => '説明1'),
-//        array($update_id = 9, array('name' => 'カードA', 'description' => '説明A')),
-//        '存在しないidを更新',
-//        $expects = 1,
-//        'idが存在しない'
-//      ),
-//      array(
-//        array('name' => 'カード2', 'description' => '説明2'),
-//        array($update_id = 2, array('id' => 2, 'name' => 'カードB', 'description' => '説明B')),
-//        '更新値にidを含む',
-//        $expects = 0,
-//        'idは更新できません'
-//      ),
-//    );
-//
-//    $stub = $this->getMock('\Testing\Part3\DB');
-//
-//    foreach ($ngCaseArray as $case) {
-//      list($input, $update, $msg, $expects, $error_msg) = $case;
-//      $stub = $this->getMock('\Testing\Part3\DB')
-//                   ->expects($this->at($expects))
-//                   ->method('method_update')
-//                   ->will($this->throwException(new \InvalidArgumentException()));
-//      \Testing\Part3\DI::bind('DB', $stub);
-//      try {
-//        $this->card_manager->create($input);
-//        $this->callCardManagerUpdate($this->card_manager, $update);
-//        $this->fail($msg);
-//      } catch (Exception $e) {
-//        $this->assertEquals($error_msg, $e->getMessage(), $msg);
-//      }
-//    }
-//  }
-//
-//  private function callCardManagerUpdate(CardManager $instance, $param)
-//  {
-//    return call_user_func_array(array($instance, 'update'), $update);
-//  }
 
   public function test削除_OK()
   {
-    $stub = $this->getMock('\Testing\Part3\DB');
-    $stub->expects($this->once())
+    $msg = '削除OK';
+    $query = '';
+
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
          ->method('method_delete')
+         ->with($this->equalTo($query))
          ->will($this->returnValue(true));
-    \Testing\Part3\DI::bind('DB', $stub);
+    \Testing\Part3\DI::bind('DB', $mock);
+
     $actual = $this->card_manager->delete(1);
-    $this->assertTrue($actual, '削除OK');
+    $this->assertTrue($actual, $msg);
   }
 
   public function test削除_NG()
   {
     $msg = '削除NG';
     $error_msg = '削除失敗';
-    $stub = $this->getMock('\Testing\Part3\DB');
-    $stub->expects($this->once())
+    $query = '';
+
+    $mock = $this->getMock('\Testing\Part3\DB');
+    $mock->expects($this->once())
          ->method('method_delete')
+         ->with($this->equalTo($query))
          ->will($this->returnValue(false));
-    \Testing\Part3\DI::bind('DB', $stub);
+    \Testing\Part3\DI::bind('DB', $mock);
 
     try {
       $this->card_manager->delete(1);
